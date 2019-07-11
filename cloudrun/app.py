6dc0bf9a831cc76326724ed43dc1570c8b3c7953
gadkari-vinayak <gadkarivinayak0@gmail.com>
2019-07-11 20:38:39 +0530

    d

--- cloudrun/app.py
+++ cloudrun/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     target = os.environ.get('TARGET', 'branch')
-    return 'hotfix-c {}!\n'.format(target)
+    return 'hotfix-d {}!\n'.format(target)
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
