9c0759c2eb3c846a13f9736318a0ae930a266744
gadkari-vinayak <gadkarivinayak0@gmail.com>
2019-07-17 18:05:41 +0530

    edit

--- cloudrun/cld/sqlcmd-backup.sh
+++ cloudrun/cld/sqlcmd-backup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 
-curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | tee /etc/apt/sources.list.d/msprod.list
 
 apt-get update
 
