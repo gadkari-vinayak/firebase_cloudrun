fb913c2d4c404f2b37e79a588d1fd47934f7f7c6
gadkari-vinayak <gadkarivinayak0@gmail.com>
2019-07-17 18:02:35 +0530

    edit

--- cloudrun/cld/sqlcmd-backup.sh
+++ cloudrun/cld/sqlcmd-backup.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
-#curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+
+apt-get update
+
+apt-get install mssql-tools unixodbc-dev -y
+
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
+
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+
+source ~/.bashrc
 
 sqlcmd -S 34.93.235.33 -U testuser -P testuser -Q "BACKUP DATABASE database1 TO DISK ='D:\backup\database1.bak' WITH NOFORMAT, NOINIT, SKIP, NOREWIND, NOUNLOAD, STATS = 10"
