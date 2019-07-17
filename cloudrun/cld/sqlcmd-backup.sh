23df1731ed71787d9d40a7c19c1346825b1dc0b5
gadkari-vinayak <gadkarivinayak0@gmail.com>
2019-07-17 17:59:26 +0530

    edit

--- cloudrun/cld/sqlcmd-backup.sh
+++ cloudrun/cld/sqlcmd-backup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+#curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 
 sqlcmd -S 34.93.235.33 -U testuser -P testuser -Q "BACKUP DATABASE database1 TO DISK ='D:\backup\database1.bak' WITH NOFORMAT, NOINIT, SKIP, NOREWIND, NOUNLOAD, STATS = 10"
