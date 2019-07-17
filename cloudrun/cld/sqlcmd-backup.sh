f55547dc0281edf4d0178c0f18a4fa60c2eac484
gadkari-vinayak <gadkarivinayak0@gmail.com>
2019-07-17 17:56:17 +0530

    edit

--- cloudrun/cld/sqlcmd-backup.sh
+++ cloudrun/cld/sqlcmd-backup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sqlcmd -S 34.93.246.37 -U testuser -P testuser -Q "BACKUP DATABASE database1 TO DISK ='D:\backup\database1.bak' WITH NOFORMAT, NOINIT, SKIP, NOREWIND, NOUNLOAD, STATS = 10"
+sqlcmd -S 34.93.235.33 -U testuser -P testuser -Q "BACKUP DATABASE database1 TO DISK ='D:\backup\database1.bak' WITH NOFORMAT, NOINIT, SKIP, NOREWIND, NOUNLOAD, STATS = 10"
