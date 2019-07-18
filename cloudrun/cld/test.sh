2ed339be8d403f43df8d930988d2ab65e481f957
gadkari-vinayak <gadkarivinayak0@gmail.com>
2019-07-18 18:21:19 +0530

    edit

--- cloudrun/cld/test.sh
+++ cloudrun/cld/test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo sqlcmd -S 34.93.207.87 -U testuser -P testuser -Q "BACKUP DATABASE database1 TO DISK ='D:\backup\database1.bak' WITH NOFORMAT, NOINIT, SKIP, NOREWIND, NOUNLOAD, STATS = 10"
+apt-get install mssql-tools -y
+sqlcmd
