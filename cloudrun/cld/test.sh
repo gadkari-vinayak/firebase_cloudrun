ad9bfae2e2f16daf156b73ab54e1a7c731e05b4b
gadkari-vinayak <gadkarivinayak0@gmail.com>
2019-07-18 19:05:02 +0530

    edit

--- cloudrun/cld/test.sh
+++ cloudrun/cld/test.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-echo $PATH
-sqlcmd
+sqlcmd -S 34.93.207.87 -U testuser -P testuser -Q "BACKUP DATABASE database1 TO DISK ='D:\backup\database1.bak' WITH NOFORMAT, NOINIT, SKIP, NOREWIND, NOUNLOAD, STATS = 10"
