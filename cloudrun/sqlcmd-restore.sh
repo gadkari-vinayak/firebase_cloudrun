8fcff6dd431a5e85508119c4316862ff9967f935
gadkari-vinayak <gadkarivinayak0@gmail.com>
2019-07-31 18:49:08 +0530

    edit

--- cloudrun/sqlcmd-restore.sh
+++ cloudrun/sqlcmd-restore.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sqlcmd -S 34.93.246.37 -U testuser -P testuser -Q "RESTORE DATABASE $BRANCH_NAME FROM DISK='D:\backup\database1.bak' WITH MOVE 'database1' TO 'D:\mydata\$BRANCH_NAME.mdf', MOVE 'database1_log' TO 'D:\mylog\$BRANCH_NAME_log.ldf'"
+sqlcmd -S 34.93.246.37 -U testuser -P testuser -Q "RESTORE DATABASE $db FROM DISK='D:\backup\database1.bak' WITH MOVE 'database1' TO 'D:\mydata\$db.mdf', MOVE 'database1_log' TO 'D:\mylog\$db_log.ldf'"
 
