2276f52f051056a206d5c9b8a325fda678610cd5
gadkari-vinayak <gadkarivinayak0@gmail.com>
2019-07-31 15:55:32 +0530

    sql

--- cloudrun/sqlcmd-restore.sh
+++ cloudrun/sqlcmd-restore.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sqlcmd -S 34.93.246.37 -U testuser -P testuser -Q "RESTORE DATABASE database1 FROM DISK ='D:\backup\database1.bak' WITH FILE = 1, REPLACE"
+sqlcmd -S 34.93.246.37 -U testuser -P testuser -Q "DROP DATABASE h1db"
+
+sqlcmd -S 34.93.246.37 -U testuser -P testuser -Q "RESTORE DATABASE h1db FROM DISK ='D:\backup\database1.bak' WITH FILE = 1, REPLACE"
