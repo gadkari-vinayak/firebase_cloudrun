af7ceb4e102d245bf7f9b338de7e5278174c8a95
gadkari-vinayak <gadkarivinayak0@gmail.com>
2019-07-31 20:09:47 +0530

    a

--- cloudrun/sqlcmd-backup.sh
+++ cloudrun/sqlcmd-backup.sh
@@ -2,4 +2,4 @@
 
 sqlcmd -S 34.93.207.87 -U testuser -P testuser -Q "BACKUP DATABASE database1 TO DISK ='D:\backup\database1.bak' WITH CHECKSUM, NOINIT, SKIP, NOREWIND, NOUNLOAD, STATS = 10"
 
-sleep 420
+#sleep 420
--- cloudrun/sqlcmd-restore.sh
+++ cloudrun/sqlcmd-restore.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-sqlcmd -S 34.93.246.37 -U testuser -P testuser -Q "RESTORE DATABASE $db FROM DISK='D:\backup\database1.bak' WITH MOVE 'database1' TO 'D:\mydata\ $db.mdf', MOVE 'database1_log' TO 'D:\mylog\ $db.ldf'"
+#echo "Positional Parameters"
+#echo '$0 = ' $1
+sqlcmd -S 34.93.246.37 -U testuser -P testuser -Q "RESTORE DATABASE $1 FROM DISK='D:\backup\database1.bak' WITH MOVE 'database1' TO 'D:\mydata\ $1.mdf', MOVE 'database1_log' TO 'D:\mylog\ $1.ldf'"
 
